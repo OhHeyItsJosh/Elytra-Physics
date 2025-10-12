c8ba50d21c33ce0b3e9b4548c16a364a2f2ee86e
ImJoshh <96638166+OhHeyItsJosh@users.noreply.github.com>
2025-10-12 18:38:19 +0100

    fixed bool config option styling being applied to all yes and no text components

--- fabric/src/main/java/me/ImJoshh/elytra_physics/config/ui/widget/BooleanConfigValue.java
+++ fabric/src/main/java/me/ImJoshh/elytra_physics/config/ui/widget/BooleanConfigValue.java
@@ -7,7 +7,6 @@ import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 
 public class BooleanConfigValue extends ConfigValue<CycleButton<Boolean>, Boolean> {
@@ -18,8 +17,8 @@ public class BooleanConfigValue extends ConfigValue<CycleButton<Boolean>, Boolea
         super(key, defaultValue, minecraft);
 
         CycleButton.Builder<Boolean> builder = CycleButton.builder((val) -> {
-            if (val) return ((MutableComponent) CommonComponents.GUI_YES).setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN));
-            else return ((MutableComponent) CommonComponents.GUI_NO).setStyle(Style.EMPTY.withColor(ChatFormatting.RED));
+            if (val) return CommonComponents.GUI_YES.copy().setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN));
+            else return CommonComponents.GUI_NO.copy().setStyle(Style.EMPTY.withColor(ChatFormatting.RED));
         });
 
         this.toggleButton = builder
