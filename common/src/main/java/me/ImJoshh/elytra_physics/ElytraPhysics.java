84799a99b54b8ee3f80fee14493108db667ebda9
ImJoshh <96638166+OhHeyItsJosh@users.noreply.github.com>
2026-06-22 03:45:25 +0100

    improvements to elytra transform to match latest versions

--- common/src/main/java/me/ImJoshh/elytra_physics/ElytraPhysics.java
+++ common/src/main/java/me/ImJoshh/elytra_physics/ElytraPhysics.java
@@ -7,6 +7,7 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
+import org.joml.Quaternionf;
 
 public class ElytraPhysics {
     public static final String MOD_ID = "elytra_physics";
@@ -24,41 +25,50 @@ public class ElytraPhysics {
     public static void applyMovementTransformation(PoseStack matrixStack, LivingEntity livingEntity, float partialTicks)
     {
         if (livingEntity instanceof AbstractClientPlayer abstractClientPlayerEntity) {
-            if (!livingEntity.isCrouching() && !livingEntity.isFallFlying()) {
-                // mostly the same as the cape render layer
-                double distX = Mth.lerp(partialTicks, abstractClientPlayerEntity.xCloakO, abstractClientPlayerEntity.xCloak) - Mth.lerp(partialTicks, abstractClientPlayerEntity.xo, abstractClientPlayerEntity.getX());
-                double distY = Mth.lerp(partialTicks, abstractClientPlayerEntity.yCloakO, abstractClientPlayerEntity.yCloak )- Mth.lerp(partialTicks, abstractClientPlayerEntity.yo, abstractClientPlayerEntity.getY());
-                double distZ = Mth.lerp(partialTicks, abstractClientPlayerEntity.zCloakO, abstractClientPlayerEntity.zCloak) - Mth.lerp(partialTicks, abstractClientPlayerEntity.zo, abstractClientPlayerEntity.getZ());
-                float yBodyRot = Mth.rotLerp(partialTicks, abstractClientPlayerEntity.yBodyRotO, abstractClientPlayerEntity.yBodyRot);
-                double forwardX = Mth.sin(yBodyRot * 0.017453292F);
-                double forwardZ = (-Mth.cos(yBodyRot * 0.017453292F));
+            // calculate cape flap and lean
+            double distX = Mth.lerp(partialTicks, abstractClientPlayerEntity.xCloakO, abstractClientPlayerEntity.xCloak) - Mth.lerp(partialTicks, abstractClientPlayerEntity.xo, abstractClientPlayerEntity.getX());
+            double distY = Mth.lerp(partialTicks, abstractClientPlayerEntity.yCloakO, abstractClientPlayerEntity.yCloak )- Mth.lerp(partialTicks, abstractClientPlayerEntity.yo, abstractClientPlayerEntity.getY());
+            double distZ = Mth.lerp(partialTicks, abstractClientPlayerEntity.zCloakO, abstractClientPlayerEntity.zCloak) - Mth.lerp(partialTicks, abstractClientPlayerEntity.zo, abstractClientPlayerEntity.getZ());
+            float yBodyRot = Mth.rotLerp(partialTicks, abstractClientPlayerEntity.yBodyRotO, abstractClientPlayerEntity.yBodyRot);
+            double forwardX = Mth.sin(yBodyRot * 0.017453292F);
+            double forwardZ = (-Mth.cos(yBodyRot * 0.017453292F));
 
-                float capeFlap = (float)distY * 10.0F;
-                capeFlap = Mth.clamp(capeFlap, -6.0F, 32.0F);
-                float capeLean = (float)(distX * forwardX + distZ * forwardZ) * 100.0F;
-                capeLean = Mth.clamp(capeLean, 0.0F, 150.0F);
-                float capeLean2 = (float)(distX * forwardZ - distZ * forwardX) * 100.0F;
-                capeLean2 = Mth.clamp(capeLean2, -20.0F, 20.0F);
-                if (capeLean < 0.0F) {
-                    capeLean = 0.0F;
-                }
-
-                float pow = Mth.lerp(partialTicks, abstractClientPlayerEntity.oBob, abstractClientPlayerEntity.bob);
-                capeFlap += Mth.sin(Mth.lerp(partialTicks, abstractClientPlayerEntity.walkDistO, abstractClientPlayerEntity.walkDist) * 6.0F) * 32.0F * pow;
-
-                float declineFactor = 0.2f * getConfig().getDeclineMultiplier();
-                matrixStack.translate(0, declineFactor * (capeLean / 150), 0);
-
-                float leanMultiplier = getConfig().getLeanMultiplier();
-                float leanMultiplierAdjusted = !abstractClientPlayerEntity.isVisuallySwimming()
-                        ? leanMultiplier * 0.85f
-                        : leanMultiplier * 0.25f;
-
-                matrixStack.mulPose(Axis.XP.rotationDegrees((leanMultiplierAdjusted * capeLean) / 2.0F + capeFlap));
-                matrixStack.mulPose(Axis.YP.rotationDegrees((capeLean2 * getConfig().getRollMultiplier()) / 2.0F));
-                matrixStack.mulPose(Axis.ZP.rotationDegrees((capeLean2 * getConfig().getPanMultiplier()) / 2.0F));
+            float capeFlap = (float)distY * 10.0F;
+            capeFlap = Mth.clamp(capeFlap, -6.0F, 32.0F);
+            float capeLean = (float)(distX * forwardX + distZ * forwardZ) * 100.0F;
+            capeLean = Mth.clamp(capeLean, 0.0F, 150.0F);
+            float capeLean2 = (float)(distX * forwardZ - distZ * forwardX) * 100.0F;
+            capeLean2 = Mth.clamp(capeLean2, -20.0F, 20.0F);
+            if (capeLean < 0.0F) {
+                capeLean = 0.0F;
             }
 
+            float pow = Mth.lerp(partialTicks, abstractClientPlayerEntity.oBob, abstractClientPlayerEntity.bob);
+            capeFlap += Mth.sin(Mth.lerp(partialTicks, abstractClientPlayerEntity.walkDistO, abstractClientPlayerEntity.walkDist) * 6.0F) * 32.0F * pow;
+
+            // apply decline
+            float declineFactor = 0.2f * getConfig().getDeclineMultiplier();
+            matrixStack.translate(0, declineFactor * (capeLean / 150), 0);
+
+            // create transform
+            float leanMultiplier = getConfig().getLeanMultiplier();
+            float leanMultiplierAdjusted = !abstractClientPlayerEntity.isVisuallySwimming()
+                    ? leanMultiplier * 0.85f
+                    : leanMultiplier * 0.25f;
+
+            Quaternionf transformation = (new Quaternionf())
+                    .rotateX(((leanMultiplierAdjusted * capeLean) / 2.0F + capeFlap) * 0.017453292F) // TILT
+                    .rotateZ(((capeLean2 * getConfig().getPanMultiplier()) / 2.0F) * 0.017453292F) // PAN
+                    .rotateY(((-capeLean2 * getConfig().getRollMultiplier()) / 2.0F) * 0.017453292F); // ROLL
+
+            // fade transformation out while flying
+            if (livingEntity.isFallFlying()) {
+                float fallFlyingTicks = livingEntity.getFallFlyingTicks() + partialTicks;
+                float fallFlyingScale = Mth.clamp(fallFlyingTicks * fallFlyingTicks / 100.0F, 0.0F, 1.0F);
+                transformation = transformation.slerp(new Quaternionf(), fallFlyingScale);
+            }
+
+            matrixStack.mulPose(transformation);
         }
     }
 
