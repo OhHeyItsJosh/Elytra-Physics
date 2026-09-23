4e0a2b936ac92c5ed6e4ebd7433ae679839f6b54
ImJoshh <96638166+OhHeyItsJosh@users.noreply.github.com>
2026-09-23 01:03:26 +0100

    updated to 26.3

--- common/src/main/java/me/ImJoshh/elytra_physics/ElytraPhysics.java
+++ common/src/main/java/me/ImJoshh/elytra_physics/ElytraPhysics.java
@@ -55,7 +55,7 @@ public class ElytraPhysics {
         float declineFactor = 0.2f * getConfig().getDeclineMultiplier();
         matrixStack.translate(0.0f, declineFactor * (avatarState.capeLean / 150.0f), 0.0f);
 
-        matrixStack.mulPose(transformation);
+        matrixStack.rotate(transformation);
     }
 
     public static float setWingSpread(AvatarRenderState avatarState) {
